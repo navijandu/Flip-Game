090064fe804a454631043ae3bd6415e2f50e8f5a
Ashmeet Singh <ashmeetsingh2010@gmail.com>
2026-03-24 12:11:47 -0400

    Update main.js

--- Jquery/main.js
+++ Jquery/main.js
@@ -1,150 +1,224 @@
 $(document).ready(function () {
-    var arrayNum = [
-        'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg', 'https://miro.medium.com/max/10000/0*wZAcNrIWFFjuJA78', 'https://media-exp1.licdn.com/dms/image/C4D03AQHX7pCBVKSZBQ/profile-displayphoto-shrink_200_200/0/1541620784757?e=1616025600&v=beta&t=pDZzQwMsIWcFLJJEuQKecMRW43JZxkysrgUSJvB3tII', 'https://pbs.twimg.com/profile_images/960684957055164416/qcflQM0w_400x400.jpg', 'https://media-cdn.tripadvisor.com/media/photo-s/12/0b/9f/a2/getlstd-property-photo.jpg', 'https://cdn.pixabay.com/photo/2013/07/18/10/56/railroad-163518__340.jpg', 'https://cdn.shopify.com/s/files/1/3026/6974/files/happy-alpacas-landscape_1024x1024.jpg?v=1532619630', 'https://www.advisor.ca/wp-content/uploads/sites/5/2018/08/800x600_Toronto_city_56928429_123RFStockPhoto.jpg', 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg', 'https://miro.medium.com/max/10000/0*wZAcNrIWFFjuJA78', 'https://media-exp1.licdn.com/dms/image/C4D03AQHX7pCBVKSZBQ/profile-displayphoto-shrink_200_200/0/1541620784757?e=1616025600&v=beta&t=pDZzQwMsIWcFLJJEuQKecMRW43JZxkysrgUSJvB3tII', 'https://pbs.twimg.com/profile_images/960684957055164416/qcflQM0w_400x400.jpg', 'https://media-cdn.tripadvisor.com/media/photo-s/12/0b/9f/a2/getlstd-property-photo.jpg', 'https://cdn.pixabay.com/photo/2013/07/18/10/56/railroad-163518__340.jpg', 'https://cdn.shopify.com/s/files/1/3026/6974/files/happy-alpacas-landscape_1024x1024.jpg?v=1532619630', 'https://www.advisor.ca/wp-content/uploads/sites/5/2018/08/800x600_Toronto_city_56928429_123RFStockPhoto.jpg'
-    ]
+    const images = [
+        "https://picsum.photos/id/1003/300/200",
+        "https://picsum.photos/id/1015/300/200",
+        "https://picsum.photos/id/1016/300/200",
+        "https://picsum.photos/id/1020/300/200",
+        "https://picsum.photos/id/1024/300/200",
+        "https://picsum.photos/id/1027/300/200",
+        "https://picsum.photos/id/1035/300/200",
+        "https://picsum.photos/id/1039/300/200"
+    ];
 
-    $(".play").click(function () {
-        if ($(".content").hasClass("clicked")) {
+    let gameImages = [...images, ...images];
+    let firstCard = null;
+    let secondCard = null;
+    let lock = false;
+    let points = 0;
+    let clicks = 0;
+    let time = 0;
+    let timer = null;
+    let previewTimer = null;
+    let gameStarted = false;
 
-        } else {
-            /*\\   delare the array*/
-            points = 0; /*set pointes 0 default*/
-            clicks = 0; /*set score 0 by default*/
-            timer = 3;
-            t = 0;
-            /*Shuffe the array randomaly */
-            function shuffle(array) {
-                array.sort(() => Math.random() - 0.5);
-            }
-            shuffle(arrayNum); /*Shuffle funtion*/
-            /* creating box */
-            for (i = 0; i < arrayNum.length; i++) {
-                $(".flip-card-inner").append(' <div class="flip-card-inner2 rollover"><div class="flip-card-front "> <img src="https://i.pinimg.com/736x/f3/09/28/f30928e4df8826714ada61ef5790eadf.jpg" ></div><div class="flip-card-back" > <img src="' + arrayNum[i] + '" > </div></div>');
-            }
-            $(".content").addClass("clicked")
-            /*time taken*/
+    function getBestScore() {
+        return parseInt(localStorage.getItem("memoryBestScore")) || 0;
+    }
 
-            setInterval(function () {
-                t = t + 1
-                $("#time").text("0 hr " + t + " Sec");
-            }, 1000);
-            /*this is for starting few sec to view the back of box's*/
-            var myinterval = setInterval(function () {
-                if (timer == 0) {
-                    clearInterval(myinterval)
-                } else {
-                    timer = timer - 1;
-                    $("#timer").text(timer)
-                }
-            }, 1000)
-            setTimeout(function () {
-                $(".flip-card-inner .flip-card-inner2").each(function () {
-                    $(this).removeClass("rollover");
-                })
-                $(".overLay").fadeOut();
-            }, 3000)
+    function setBestScore(score) {
+        localStorage.setItem("memoryBestScore", score);
+        $("#bestScore").text(score);
+    }
+
+    function loadBestScore() {
+        $("#bestScore").text(getBestScore());
+    }
+
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
         }
+    }
 
-        /*Addind Audio*/
-        var audioElement = document.createElement('audio');
-        audioElement.setAttribute('src', 'http://p128.ve.vc/data/48/37057/278600/Bring%20The%20Pain%20-%20Sound%20Shikari%20(DjPunjab.Com).mp3');
-        audioElement.addEventListener('ended', function () {
-            this.play();
-        }, false);
+    function buildBoard() {
+        shuffle(gameImages);
+        $("#gameBoard").html("");
 
-        audioElement.play();
-        $("#play").click(function () {
-            audioElement.play();
-        })
-
-        $("#pause").click(function () {
-            audioElement.pause();
-        })
-
-
-
-
-        /*click on box to rotate*/
-        $(".flip-card-inner2").click(function (index) {
-            var checkArray = []
-            $(".flip-card-inner .flip-card-inner2").each(function () {
-                if ($(this).hasClass("done")) {
-                    checkArray.push(1)
-                }
-            })
-            if (checkArray.length == arrayNum.length) {
-                /*calulate if all the boxes are revieled */
-                alert("Thanks for playing ")
-            } else {
-                clicks = clicks + 1; /*  Increasing the clicks */
-                $("#click").text(clicks);
-
-                var elmId = $(this).attr('id');
-                if ($(this).hasClass("done")) {} else {
-                    var firstClick = $(this) /*first click this declare */
-                    $(this).addClass("rollover");
-                    var r = $(this).find(".flip-card-back img").attr("src"); /* first vale on first click*/
-
-                    var currentIndex = $(this).index(); /*index of the box */
-
-                    $(".flip-card-inner .flip-card-inner2").each(function (index) {
-                        if (currentIndex == $(this).index()) {
-                            /*nothing will happen */
-                        } else {
-                            if ($(this).hasClass("rollover")) {
-                                var r2 = $(this).find(".flip-card-back img").attr("src"); /*value of 2nd clcik*/
-
-                                if (r == r2) {
-                                    points = points + 100; /*calclulate points on match */
-                                    $("#points").text(points);
-
-                                    var thisele = $(this) /*  2nd clcik to get the 2nd val */
-                                    setTimeout(function () {
-                                        thisele.addClass("done").removeClass("rollover");
-                                        firstClick.addClass("done").removeClass("rollover");
-                                    }, 1000)
-                                } else {
-                                    if (points == 0) {
-
-                                    } else {
-                                        points = points - 20;
-                                        $("#points").text(points);
-                                    }
-
-                                    var thisele = $(this)
-                                    setTimeout(function () {
-                                        thisele.removeClass("rollover");
-                                        firstClick.removeClass("rollover");
-
-                                    }, 800)
-                                }
-                            }
-                        }
-                    })
-                }
-            }
+        gameImages.forEach((img) => {
+            $("#gameBoard").append(`
+                <div class="flip-card-inner2" data-img="${img}">
+                    <div class="flip-card-front">
+                        <img src="https://cdn-icons-png.flaticon.com/512/5726/5726532.png" alt="Card Front">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="${img}" alt="Card Back">
+                    </div>
+                </div>
+            `);
         });
-    });
-    $("#reload").click(function () {
-        location.reload();
+    }
+
+    function startTimer() {
+        clearInterval(timer);
+        timer = setInterval(function () {
+            time++;
+            $("#time").text(time + " Sec");
+        }, 1000);
+    }
+
+    function startPreview() {
+        let count = 3;
+        $("#timer").text(count);
+        $(".overLay").show();
+        $(".flip-card-inner2").addClass("rollover");
+
+        clearInterval(previewTimer);
+        previewTimer = setInterval(function () {
+            count--;
+            $("#timer").text(count);
+
+            if (count <= 0) {
+                clearInterval(previewTimer);
+                $(".flip-card-inner2").removeClass("rollover");
+                $(".overLay").fadeOut();
+            }
+        }, 1000);
+    }
+
+    function resetState() {
+        firstCard = null;
+        secondCard = null;
+        lock = false;
+    }
+
+    function updateBestIfNeeded() {
+        const bestScore = getBestScore();
+        if (points > bestScore) {
+            setBestScore(points);
+        }
+    }
+
+    function showWinPopup() {
+        $("#finalStats").html(
+            "Points: <strong>" + points + "</strong><br>" +
+            "Turns: <strong>" + clicks + "</strong><br>" +
+            "Time: <strong>" + time + " Sec</strong>"
+        );
+        $("#winPopup").addClass("show");
+    }
+
+    function checkWin() {
+        if ($(".done").length === gameImages.length) {
+            clearInterval(timer);
+            gameStarted = false;
+            updateBestIfNeeded();
+            setTimeout(function () {
+                showWinPopup();
+            }, 350);
+        }
+    }
+
+  $(document).on("click", ".flip-card-inner2", function () {
+    if (!gameStarted) return;
+    if (lock) return;
+    if ($(this).hasClass("done")) return;
+    if ($(".overLay").is(":visible")) return;
+
+    if (firstCard && !secondCard && $(this).is(firstCard)) {
+        $(this).removeClass("rollover");
+        firstCard = null;
+        return;
+    }
+
+    if ($(this).hasClass("rollover")) return;
+
+    $(this).addClass("rollover");
+
+    if (!firstCard) {
+        firstCard = $(this);
+        return;
+    }
+
+    secondCard = $(this);
+    lock = true;
+
+    clicks++;
+    $("#click").text(clicks);
+
+    let img1 = firstCard.data("img");
+    let img2 = secondCard.data("img");
+
+    if (img1 === img2) {
+        points += 100;
+        $("#points").text(points);
+
+        setTimeout(function () {
+            firstCard.addClass("done rollover");
+            secondCard.addClass("done rollover");
+            resetState();
+            checkWin();
+        }, 500);
+    } else {
+        points = Math.max(0, points - 20);
+        $("#points").text(points);
+
+        setTimeout(function () {
+            firstCard.removeClass("rollover");
+            secondCard.removeClass("rollover");
+            resetState();
+        }, 700);
+    }
+});
+
+    function startGame() {
+        clearInterval(timer);
+        clearInterval(previewTimer);
+
+        gameStarted = true;
+        points = 0;
+        clicks = 0;
+        time = 0;
+        resetState();
+
+        $("#points").text(points);
+        $("#click").text(clicks);
+        $("#time").text("0 Sec");
+        $("#winPopup").removeClass("show");
+
+        gameImages = [...images, ...images];
+        buildBoard();
+        startPreview();
+        startTimer();
+    }
+
+    $("#startGame").click(function () {
+        startGame();
     });
 
-    /*   $("#close").click(function () {
-          self.close();
-      });*/
+    $("#reload").click(function () {
+        startGame();
+    });
+
+    $("#playAgain").click(function () {
+        startGame();
+    });
+
+    $("#close").click(function () {
+        window.close();
+    });
+
+    loadBestScore();
 });
-/*Full Screen open*/
+
 var elem = document.documentElement;
 
 function openFullscreen() {
     if (elem.requestFullscreen) {
         elem.requestFullscreen();
     } else if (elem.webkitRequestFullscreen) {
-        /* Safari */
         elem.webkitRequestFullscreen();
     } else if (elem.msRequestFullscreen) {
-        /* IE11 */
         elem.msRequestFullscreen();
     }
 }
-/*Full Screen open End*/
